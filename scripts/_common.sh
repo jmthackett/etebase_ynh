2d7bb7423094d9bd84dd115f0b6f22d3456f84b1
John Hackett <john@mutual.email>
2023-02-10 20:05:47 +0000

    adding common script

---
+++ scripts/_common.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+#PKG_DEPENDENCIES="sqlite3 imagemagick libldap2-dev libsasl2-dev python3-venv python3-dev python3-lxml libjpeg-dev zlib1g-dev libffi-dev"
+PKG_DEPENDENCIES="sqlite3 python3-pip"
+
+DOSSIER_MEDIA=/home/yunohost.multimedia
+
+#These var are used in init_calibre_db_settings conf file
+log_file=/var/log/$app/$app.log
+access_log_file=/var/log/$app/$app-access.log
+
