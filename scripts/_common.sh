8ebf9134ef8661ba92d4451287c7e8a01224c398
John Hackett <john@mutual.email>
2023-02-13 19:17:19 +0000

    frustratedly flip flopping back to postgres

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-PKG_DEPENDENCIES="python3-pip python3 python3-venv python3-dev python3-lxml default-libmysqlclient-dev build-essential libmariadb-dev-compat libmariadb-dev libssl-dev libssl1.1"
+PKG_DEPENDENCIES="python3-pip python3 python3-venv python3-dev python3-lxml python3-psycopg2"
 #PKG_DEPENDENCIES="sqlite3 python3-pip"
 
 DOSSIER_MEDIA=/home/yunohost.multimedia
@@ -9,3 +9,55 @@ DOSSIER_MEDIA=/home/yunohost.multimedia
 log_file=/var/log/$app/$app.log
 access_log_file=/var/log/$app/$app-access.log
 
+
+#=================================================
+# EXPERIMENTAL HELPERS
+#=================================================
+
+#=================================================
+#
+# Redis HELPERS
+#
+# Point of contact : Jean-Baptiste Holcroft <jean-baptiste@holcroft.fr>
+#
+# Copied from: https://github.com/YunoHost-Apps/funkwhale_ynh/blob/master/scripts/_common.sh
+#
+#=================================================
+
+# get the first available redis database
+#
+# usage: ynh_redis_get_free_db
+# | returns: the database number to use
+ynh_redis_get_free_db() {
+	local result max db
+	result=$(redis-cli INFO keyspace)
+
+	# get the num
+	max=$(cat /etc/redis/redis.conf | grep ^databases | grep -Eow "[0-9]+")
+
+	db=0
+	# default Debian setting is 15 databases
+	for i in $(seq 0 "$max")
+	do
+	 	if ! echo "$result" | grep -q "db$i"
+	 	then
+			db=$i
+	 		break 1
+ 		fi
+ 		db=-1
+	done
+
+	test "$db" -eq -1 && ynh_die --message="No available Redis databases..."
+
+	echo "$db"
+}
+
+# Create a master password and set up global settings
+# Please always call this script in install and restore scripts
+#
+# usage: ynh_redis_remove_db database
+# | arg: database - the database to erase
+ynh_redis_remove_db() {
+	local db=$1
+	redis-cli -n "$db" flushall
+}
